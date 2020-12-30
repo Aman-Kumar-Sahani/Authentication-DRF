fa2e1f51585ad8fe9804afc360e3e5ea487fa398
unknown <Aman7548>
2020-12-30 22:23:57 +0530

    First Commit

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path,include
-from .views import  ArticleAPIView,ArticleDetails,GenericAPIView
+from .views import  ArticleAPIView,ArticleDetails,GenericAPIView,ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article',ArticleViewSet,basename='article')
 
 urlpatterns = [
     # path('article/', article_list),
+    path('viewset/',include(router.urls)),
     path('article/', ArticleAPIView.as_view()),
     path('detail/<int:id>/', ArticleDetails.as_view()),
     path('generic/article/<int:id>/', GenericAPIView.as_view()),
--- api/views.py
+++ api/views.py
@@ -10,12 +10,45 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+# from rest_framework.authentication import SessionAuthentication , BasicAuthentication
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
-# Geneics Class Based Views  
+
+# Geneics Class Based Views  for crud operation
+
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self,request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many = True)
+        return Response(serializer.data)
+
+    def create(self,request):
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors ,status = status.HTTP_400_BAD_REQUEST)
+
+    def retreive(self,request,pk = None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset , pk = pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+
+
+
+
 class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
+    # authentication_classes = [SessionAuthentication , BasicAuthentication]
+    authentication_classes  = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self,request,id = None):
         if id:
@@ -42,7 +75,7 @@ class ArticleAPIView(APIView):
         serializer = ArticleSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status = status.HTTP_201_CREATE)
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors ,status = status.HTTP_400_BAD_REQUEST)
 
 
